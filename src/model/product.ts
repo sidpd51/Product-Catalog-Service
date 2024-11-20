ae76a405a11f0c63e67fc68e1c9ea7aa8f9f6e07
sidpd51 <sidpd510@gmail.com>
2024-11-21 02:59:23 +0530

    feat: Add product model schema and model

---
+++ src/model/product.ts
@@ -0,0 +1,70 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+export interface IProduct extends Document {
+    name: string;
+    description?: string;
+    price: number;
+    images: string[];
+    specifications: Map<string, string>;
+    categoryID: mongoose.Types.ObjectId;
+    reviews: mongoose.Types.ObjectId[];
+    rating: {
+        averageRating: number;
+        reviewCount: number;
+    },
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const productSchema: Schema<IProduct> = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        description: { type: String },
+        price: {
+            type: Number,
+            required: true,
+            min: 0
+        },
+        images: {
+            type: [String],
+            default: []
+        },
+        specifications: {
+            type: Map,
+            of: String,
+            default: {}
+        },
+        categoryID: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Category",
+            required: true,
+        }, // Reference to Category
+        reviews: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Review",
+            },
+        ],
+        rating: {
+            averageRating: {
+                type: Number,
+                default: 0,
+                min: 0,
+                max: 5
+            },
+            reviewCount: {
+                type: Number,
+                default: 0,
+                min: 0
+            },
+        },
+    },
+    { timestamps: true }
+);
+
+const Product: Model<IProduct> = mongoose.model<IProduct>("Product", productSchema);
+export default Product;
