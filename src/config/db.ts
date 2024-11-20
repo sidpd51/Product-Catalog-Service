0f527854f51d84e94be988ef2d088185e4c09c1b
sidpd51 <sidpd510@gmail.com>
2024-11-20 23:15:15 +0530

    chore: remove old setup files

--- src/config/db.ts
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const connect = async():Promise<void>=>{
-    await mongoose.connect('mongodb://localhost/Product_Catalog_Server')
-    console.log('db connected')
-}
-
-module.exports = {mongoose,connect};
