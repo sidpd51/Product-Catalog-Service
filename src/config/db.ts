9b5a18808aa67cf0ed5b0feaa8283b40510ed305
sidpd51 <sidpd510@gmail.com>
2024-11-24 18:47:58 +0530

    feat: Adding mongodb atlas and configuring it as db

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,16 @@
 import mongoose from "mongoose";
+import { PASSWORD, USERNAME } from "./serverConfig";
 
-const connect = async()=>{
-    await mongoose.connect('mongodb://localhost/Product_Catalog_Server')
-    console.log('db connected!')
+const connect = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.9owv8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/product-catalog-service`)
+        // await mongoose.connect('mongodb://localhost/Product_Catalog_Server')
+
+        console.log('db connected successfully!')
+    } catch (error) {
+        console.log(error)
+        console.log("Error connecting database!")
+    }
 }
 
 export default connect;
--- src/config/serverConfig.ts
+++ src/config/serverConfig.ts
@@ -3,3 +3,5 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const PORT:string|undefined = process.env.PORT
+export const USERNAME:string|undefined = process.env.USERNAME
+export const PASSWORD:string|undefined = process.env.PASSWORD
