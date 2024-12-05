d5d5eec4fee7ae40b24e23069054382ddd3c3415
sidpd51 <sidpd510@gmail.com>
2024-12-06 01:32:22 +0530

    chore: Add interface IReviewController and implement on class ReviewController

--- src/Controller/implementations/ReviewController.ts
+++ src/Controller/implementations/ReviewController.ts
@@ -1,8 +1,9 @@
 import { IReviewModel } from "../../model/reviewModel";
 import ReviewService from "../../service/implementations/ReviewService";
+import IGenericController from "../interfaces/IGenericController";
 import GenericController from "./GenericController";
 
-export default class ReviewController extends GenericController<IReviewModel> {
+export default class ReviewController extends GenericController<IReviewModel> implements IGenericController {
     constructor() {
         super(new ReviewService())
     }
