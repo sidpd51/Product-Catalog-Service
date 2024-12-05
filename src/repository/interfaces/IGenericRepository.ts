356af29c3086cf11c977c544f57e8641e0e6357b
sidpd51 <sidpd510@gmail.com>
2024-12-06 00:38:15 +0530

    chore: refactor IGenericRepository to extends T as Document

--- src/repository/interfaces/IGenericRepository.ts
+++ src/repository/interfaces/IGenericRepository.ts
@@ -1,6 +1,6 @@
-import { Types } from "mongoose";
+import { Document, Types } from "mongoose";
 
-export default interface IGenericRepository<T> {
+export default interface IGenericRepository<T extends Document> {
     create(data: Partial<T>): Promise<T | null>;
     getAll(): Promise<T[]>;
     update(id: Types.ObjectId | string,data: Partial<T>): Promise<T | null>;
