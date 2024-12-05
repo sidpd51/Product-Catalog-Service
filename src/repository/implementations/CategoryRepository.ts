782e88f775c4cfc39b2197038daad99888c729bd
sidpd51 <sidpd510@gmail.com>
2024-12-06 00:40:26 +0530

    chore: Add ICategoryRepository

--- src/repository/implementations/CategoryRepository.ts
+++ src/repository/implementations/CategoryRepository.ts
@@ -1,8 +1,9 @@
 import Category, { ICategory } from "../../model/catagoryModel";
+import ICategoryRepository from "../interfaces/ICategoryRepository";
 import GenericRepository from "./GenericRepository";
 
 
-export default class CategoryRepository extends GenericRepository<ICategory> {
+export default class CategoryRepository extends GenericRepository<ICategory> implements ICategoryRepository {
     constructor() {
         super(Category)
     }
---
+++ src/repository/interfaces/ICategoryRepository.ts
@@ -0,0 +1,6 @@
+import { ICategory } from "../../model/catagoryModel";
+import IGenericRepository from "./IGenericRepository";
+
+export default interface ICategoryRepository extends IGenericRepository<ICategory>{
+
+}
