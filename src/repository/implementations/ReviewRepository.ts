2e1c3cdb28256962eff115d7d575f32e4432fa53
sidpd51 <sidpd510@gmail.com>
2024-12-06 00:46:47 +0530

    chore: Add IReviewRepository interface and implement in class ReviewRepository

--- src/repository/implementations/ReviewRepository.ts
+++ src/repository/implementations/ReviewRepository.ts
@@ -1,8 +1,9 @@
 import Review, { IReview } from "../../model/reviewModel";
+import IReviewRepository from "../interfaces/IReviewRepository";
 import GenericRepository from "./GenericRepository";
 
 
-export default class ReviewRepository extends GenericRepository<IReview> {
+export default class ReviewRepository extends GenericRepository<IReview> implements IReviewRepository {
     constructor() {
         super(Review)
     }
